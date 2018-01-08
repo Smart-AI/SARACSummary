61839973424cfd1241ddcee1dd234db544e1a1d5
Smart-AI <weizhidaoshuo@gmail.com>
2018-01-08 18:05:11 +0800

    update
    Signed-off-by: Smart-AI <weizhidaoshuo@gmail.com>

---
+++ RACSequence/Pods/Target Support Files/Pods-RACSequenceTests/Pods-RACSequenceTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RACSequenceTests : NSObject
+@end
+@implementation PodsDummy_Pods_RACSequenceTests
+@end
--- RACSequence/RACSequence/ViewController.m
+++ RACSequence/RACSequence/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <ReactiveObjC/ReactiveObjC.h>
 
 @interface ViewController ()
 
@@ -17,6 +18,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (IBAction)loop:(id)sender {
+    NSString *countryFile = [[NSBundle mainBundle] pathForResource:@"country.plist" ofType:nil];
+    NSArray *dictArray = [NSArray arrayWithContentsOfFile:countryFile];
+    
+    [dictArray.rac_sequence.signal subscribeNext:^(id  _Nullable x) {
+        NSLog(@"%@",x);
+    } error:^(NSError * _Nullable error) {
+        NSLog(@"error=%@",error);
+    } completed:^{
+        NSLog(@"completed");
+    }];
+    
+    
+    
+}
+
+- (IBAction)macroLoop:(id)sender {
+    NSDictionary *dict = @{@"key1":@"1", @"key2":@"2"};
+    [dict.rac_sequence.signal subscribeNext:^(id  _Nullable x) {
+        NSLog(@"%@",x);
+        NSString *key = x[0];
+        NSString *value = x[1];
+        NSLog(@"key=%@ value=%@",key,value);
+        RACTupleUnpack(NSString *keys, NSString *values) = x;
+        NSLog(@"keys=%@ values=%@",keys,values);
+    } error:^(NSError * _Nullable error) {
+        NSLog(@"error=%@",error);
+    } completed:^{
+        NSLog(@"completed");
+    }];
 }
 
 
